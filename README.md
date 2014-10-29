77fa5b129a7cce070e89b605d8a47c22a6ca4eb8
Cédric <cedced19@gmail.com>
2014-10-29 12:27:53 +0100

    Fix a bug

---
+++ README.md
@@ -0,0 +1,6 @@
+#Irregular Verbs
+
+[![devDependency Status](https://david-dm.org/cedced19/irregular-verbs/dev-status.svg)](https://david-dm.org/cedced19/irregular-verbs#info=devDependencies)
+[![Built with Grunt](https://cdn.gruntjs.com/builtwith.png)](http://gruntjs.com/)
+
+Do `grunt` to compile.
--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,7 +1,7 @@
 angular.module("IrregularVerbs", [])
 .controller("IrregularVerbsCtrl", function($scope, $http) {
 
-        $http.get("/data.json").success(function(data, status, headers, config) {
+        $http.get("data.json").success(function(data, status, headers, config) {
 
         $scope.verbs = data;
         $scope.search = null;
